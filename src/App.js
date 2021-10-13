4e59a932263fccf28f3fb19ce550dd8c8d7c1e6d
chucky <charles.ssekitto@cobblestoneenergy.com>
2021-10-13 11:38:21 +0100

    add protected routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { Counter } from "./components/counter";
+import {
+  Home,
+  QuestionDetails,
+  LeaderBoards,
+  Login,
+  ProtectedRoute,
+} from "./components";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Counter />
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/question-details">Question Details</Link>
+            </li>
+            <li>
+              <Link to="/leader-boards">LeaderBoards</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <ProtectedRoute
+            exact
+            path="/leader-boards"
+            component={LeaderBoards}
+          />
+          <ProtectedRoute exact path="/" component={Home} />
+          <ProtectedRoute
+            exact
+            path="/question-details"
+            component={QuestionDetails}
+          />
+        </Switch>
       </div>
     </Router>
   );
---
+++ src/components/protectedRoute.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+  const user = useSelector((state) => {
+    return state.counter.loggedInUser.loggedIn;
+  });
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        if (user) {
+          return <Component {...rest} {...props} />;
+        } else {
+          return (
+            <Redirect
+              to={{
+                pathname: "/login",
+                state: {
+                  from: props.location,
+                },
+              }}
+            />
+          );
+        }
+      }}
+    />
+  );
+};
+
+export default ProtectedRoute;
