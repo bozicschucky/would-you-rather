311ada718848e18f98e45a8d0e436fe3ee06103d
chucky <charles.ssekitto@cobblestoneenergy.com>
2021-10-15 07:17:15 +0100

    add the question details logic

--- src/components/questionDetails.jsx
+++ src/components/questionDetails.jsx
@@ -1,5 +1,6 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { saveQuestionAnswer, getQuestions } from "../store/reducers";
 
 const computePercentage = (votes, totalVotes) => {
   if (votes === 0) {
@@ -8,10 +9,12 @@ const computePercentage = (votes, totalVotes) => {
   return Math.round((votes / totalVotes) * 100);
 };
 export const QuestionDetails = (props) => {
+  const dispatch = useDispatch();
   const questionId = props.match.params.id;
   const questions = useSelector((state) => state.counter.questions);
   const authorDetails = useSelector((state) => state.counter.users);
-
+  const loggedInUser = useSelector((state) => state.counter.loggedInUser.name);
+  const [questionAnswer, setQuestionAnswer] = useState({});
   const selectedQuestion = questions[questionId];
   const pollAuthor = selectedQuestion.author;
   const authorDetail = authorDetails[pollAuthor];
@@ -19,6 +22,33 @@ export const QuestionDetails = (props) => {
   const optionTwoVotes = selectedQuestion.optionTwo.votes.length;
   const selectedOptionOnePollText = selectedQuestion.optionOne.text;
   const selectedOptionTwoPollText = selectedQuestion.optionTwo.text;
+
+  let selectedQuestionCopy = {};
+
+  useEffect(() => {
+    if (questionAnswer.hasOwnProperty("qid")) {
+      dispatch(saveQuestionAnswer(questionAnswer));
+      dispatch(getQuestions(questionAnswer));
+    }
+  }, [dispatch, questionAnswer]);
+  const handleSelectedOption = (e) => {
+    const selectedOption = e.target.value;
+    if (selectedOption === "optionOne") {
+      selectedQuestionCopy = {
+        answer: selectedOption,
+        authedUser: loggedInUser,
+        qid: questionId,
+      };
+      setQuestionAnswer(selectedQuestionCopy);
+    } else {
+      selectedQuestionCopy = {
+        answer: selectedOption,
+        authedUser: loggedInUser,
+        qid: questionId,
+      };
+      setQuestionAnswer(selectedQuestionCopy);
+    }
+  };
   const optionOneVoters = JSON.stringify(selectedQuestion.optionOne.votes);
   const optionTwoVoters = JSON.stringify(selectedQuestion.optionTwo.votes);
 
@@ -27,21 +57,37 @@ export const QuestionDetails = (props) => {
       <h3>Welcome to the Poll {questionId}</h3>
       <div key={selectedQuestion.id} className="card options-card">
         <p>Would you Rather</p>
-        <img src={authorDetail["avatarURL"]} alt="author images"></img>
+        <img
+          src={authorDetail["avatarURL"]}
+          alt="author images"
+          className="author-image"
+        ></img>
         <div className="options-holder">
           <div className="option-text">
             <p>
               {selectedOptionOnePollText} || {selectedOptionTwoPollText}
             </p>
             <p>
-              {optionOneVotes} votes || {optionTwoVotes} votes
+              {optionOneVotes}/3 votes || {optionTwoVotes}/3 votes
             </p>
             <p>
               {computePercentage(optionOneVotes, 3)}% ||{" "}
               {computePercentage(optionTwoVotes, 3)} %
             </p>
+            <div>
+              <label htmlFor="options">Vote:</label>
+              <select
+                id="options"
+                defaultValue="choose between the options"
+                onChange={handleSelectedOption}
+              >
+                <option value="">vote</option>
+                <option value="optionOne">Option one</option>
+                <option value="optionTwo">Option two</option>
+              </select>
+            </div>
             <p>
-              {optionOneVoters} || {optionTwoVoters}
+              Option voters: {optionOneVoters} || {optionTwoVoters}
             </p>
           </div>
           <p>Written by {pollAuthor}</p>
